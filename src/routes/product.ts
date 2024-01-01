a204b8dda7b3205b943c405478e3e76561dbb7e1
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2024-01-01 16:50:46 +0700

    add logger and handle body request

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -1,7 +1,14 @@
 import { Router, NextFunction, Request, Response } from 'express'
+import { logger } from '../utils/logger'
 
 export const ProductRouter: Router = Router()
 
 ProductRouter.get('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Success get product data')
   res.status(200).send({ status: true, statusCode: 200, data: [{ name: 'sepatu', price: 500000 }] })
+})
+
+ProductRouter.post('/', (req: Request, res: Response, next: NextFunction) => {
+  logger.info('Success post new product')
+  res.status(200).send({ status: true, statusCode: 200, data: req.body })
 })
