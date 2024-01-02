110bdd5a3f0973ca1864434187f7ee8f22a1e9cc
Muhammad Farhan Hamidie <hamidie.farhan@gmail.com>
2024-01-02 20:47:09 +0700

    feat: add data to DB

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -1,47 +1,44 @@
 import { Request, Response } from 'express'
 import { createProductValidation } from '../validations/product.validation'
 import { logger } from '../utils/logger'
-import { getProductFromDB } from '../services/product.services'
+import { addProductToDB, getProductByID, getProductFromDB } from '../services/product.services'
+import { v4 as uuidv4 } from 'uuid'
 
-interface ProductType {
-  product_id: string
-  name: string
-  price: number
-  size: string
-}
-
-export const createProduct = (req: Request, res: Response) => {
+export const createProduct = async (req: Request, res: Response) => {
+  req.body.product_id = uuidv4()
   const { error, value } = createProductValidation(req.body)
   if (error) {
     logger.error('Err = product-create', error.details[0].message)
-    return res.status(422).send({ status: false, statuseCode: 422, message: error.details[0].message, data: {} })
+    return res.status(422).send({ status: false, statuseCode: 422, message: error.details[0].message })
   }
-  logger.info('Success post new product')
-  return res.status(200).send({ status: true, statusCode: 200, message: 'Add Product Success', data: value })
+  try {
+    await addProductToDB(value)
+    logger.info('Success post new product')
+    return res.status(201).send({ status: true, statusCode: 201, message: 'Add Product Success' })
+  } catch (error) {
+    logger.error('Err = product-create', error)
+    return res.status(422).send({ status: false, statuseCode: 422, message: error })
+  }
 }
 
 export const getProduct = async (req: Request, res: Response) => {
-  const products: any = await getProductFromDB()
-
   const {
-    params: { name }
+    params: { id }
   } = req
 
-  if (name) {
-    const filterProduct = products.filter((product: ProductType) => {
-      if (product.name === name) {
-        return product
-      }
-    })
-    if (filterProduct.length === 0) {
-      logger.info('Data not found')
-      return res.status(200).send({ status: false, statusCode: 404, data: {} })
+  if (id) {
+    const product = await getProductByID(id)
+    if (product) {
+      logger.info('Success get product data')
+      return res.status(200).send({ status: true, statusCode: 200, data: product })
+    } else {
+      return res.status(200).send({ status: true, statusCode: 404, message: 'Data not found', data: {} })
     }
+  } else {
+    const products: any = await getProductFromDB()
     logger.info('Success get product data')
-    return res.status(200).send({ status: true, statusCode: 200, data: filterProduct[0] })
+    return res.status(200).send({ status: true, statusCode: 200, data: products })
   }
-  logger.info('Success get product data')
-  return res.status(200).send({ status: true, statusCode: 200, data: products })
 }
 
 /*
